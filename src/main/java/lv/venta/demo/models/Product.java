21f409972d6c34cb0815dbb28b988bf7e35eb76f
BadMoodDuck <2dwarriorjonny@gmail.com>
2022-05-13 22:17:30 +0300

    Edit: Product class with adding validation

--- src/main/java/lv/venta/demo/models/Product.java
+++ src/main/java/lv/venta/demo/models/Product.java
@@ -1,13 +1,32 @@
 package lv.venta.demo.models;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
+
 public class Product {
 	
 	//id,title,description,quantity,price
 	private int id;
+	
+	@Size(min=4, max=20)
+	@Pattern(regexp="[A-Z]{1}[a-z]+")
 	private String title;
+	
+	@Size(min=5, max=20)
+	@Pattern(regexp="[A-Z]{1}[a-z\\s]+")
 	private String description;
+	
+	@Min(value=1)
+	@Max(value=1000)
 	private int quantity;
+	
+	@Min(value=0)
+	@Max(value=10000)
 	private float price;
+	
+	
 	public static int counter = 1;
 	
 	//getters
